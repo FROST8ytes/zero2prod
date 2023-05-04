70cfd3cdd90afdd57690798658e8723916aa22b5
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2023-05-04 12:52:41 +0800

    Change port from 5432 to 65535

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -23,14 +23,14 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=65535}"
 
 # Launch postgres using Docker
 docker run \
   -e POSTGRES_USER=${DB_USER} \
   -e POSTGRES_PASSWORD=${DB_PASSWORD} \
   -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
+  -p "${DB_PORT}":65535 \
   -d postgres \
   postgres -N 1000
 
